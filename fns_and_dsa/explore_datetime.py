deefab018dc7d5c391541433b772a1cc1bf12d28
loreahlourien-sudo <loreahlourien@gmail.com>
2025-09-28 12:48:05 +0300

    file containing calculation of date and time

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,14 @@
+import datetime
+def display_current_datetime ():
+ current_date = datetime.datetime.now()
+print("Current date and time:",current_date)
+
+
+
+number_of_days = int(input("Enter the number of days :"))
+datetime.timedelta(days=number_of_days) 
+
+def calculate_futuretime_date():
+    future_date = display_current_datetime + number_of_days
+print()
+   
