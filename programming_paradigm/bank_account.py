cbfd5abfc2e4705326bbf613eecf919cbed076ac
loreahlourien-sudo <loreahlourien@gmail.com>
2025-10-05 14:42:12 +0300

    new directory added with a file containig files

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,18 @@
+
+class BankAccount ():
+    def  __init__ (self, initial_balance= 0):
+        if initial_balance < 0:
+            raise ValueError ("Initial balance cannot be negative .")
+        self.account_balance = initial_balance
+    def deposit (self, amount):
+        if amount > 0 :
+            self.account_balance += amount
+            return True
+        return False
+    def withdraw (self, amount):
+        if amount > 0 and self.account_balance >= amount:
+            self.account_balance -= amount
+            return True
+        return False
+    def display_balance(self):
+        print(f"Current balance:${self.account_balance:.2f}")
